b0f70b136748ebed89bda1cc29f533905c73a559
Vanessa <vanessa.wiedeman.cs@gmail.com>
2019-10-22 18:21:43 -0500

    Trivia game design. All elements showing for now. HTML/CSS foundation.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,14 @@
+/*Javascript Header
+Description: In this program we will be using setTimeout() method to make a working 
+trivia game. 
+*/
+
+var quiz =  ["Yes or no", "Yes", "No"]
+            ["No or yes", "Yes", "No"]
+            ["Question NO", "yes", "NO"];
+
+
+
 
 
 
