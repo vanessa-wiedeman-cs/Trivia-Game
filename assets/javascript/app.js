ec424c1a9f63b911d91a753594786da0ce004b6f
Vanessa <vanessa.wiedeman.cs@gmail.com>
2019-10-22 19:24:17 -0500

    pushing error

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,13 +3,33 @@ Description: In this program we will be using setTimeout() method to make a work
 trivia game. 
 */
 
-var quiz =  ["Yes or no", "Yes", "No"]
-            ["No or yes", "Yes", "No"]
-            ["Question NO", "yes", "NO"];
+//variables
+var quiz = [
+    "The pre evolution of pikachu is...",
+    "Pikachu",
+    "Pichu",
+    "Raichu",
+    "Jigglypuff",
+][
+    ("The first legendary Ash saw was...",
+    "Mewtwo",
+    "Mew",
+    "Jigglypuff",
+    "Ho oh")
+][
+    ("The pokemon rap references how many pokemon...",
+    "150+",
+    "200+",
+    "250+",
+    "300+")
+];
 
+var test = 0;
 
-
-
-
-
-
+//Document read functions
+$(document).ready(function() {
+   alert("ahhh");
+    $("#game").on("click", function() {
+        alert("AH");
+    });
+});
