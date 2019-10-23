d3f6162d6d0889e814571b51a1715c6f4b71bcf9
Vanessa <vanessa.wiedeman.cs@gmail.com>
2019-10-22 19:44:56 -0500

    error gone

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,31 +5,34 @@ trivia game.
 
 //variables
 var quiz = [
-    "The pre evolution of pikachu is...",
-    "Pikachu",
-    "Pichu",
-    "Raichu",
-    "Jigglypuff",
-][
-    ("The first legendary Ash saw was...",
-    "Mewtwo",
-    "Mew",
-    "Jigglypuff",
-    "Ho oh")
-][
-    ("The pokemon rap references how many pokemon...",
-    "150+",
-    "200+",
-    "250+",
-    "300+")
+    [
+        "The pre evolution of pikachu is...",
+        "Pikachu",
+        "Pichu",
+        "Raichu",
+        "Jigglypuff",
+    ],
+    [
+        "The first legendary Ash saw was...",
+        "Mewtwo",
+        "Mew",
+        "Jigglypuff",
+        "Ho oh",
+    ],
+    [
+        "The pokemon rap references how many pokemon...",
+        "150+",
+        "200+",
+        "250+",
+        "300+",
+    ],
 ];
 
 var test = 0;
 
 //Document read functions
 $(document).ready(function() {
-   alert("ahhh");
     $("#game").on("click", function() {
-        alert("AH");
+
     });
 });
